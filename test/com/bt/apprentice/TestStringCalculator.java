fea41b763f1b2b3be135699eb79741575872b8f6
Kerry Buckley <kerryjbuckley@gmail.com>
2013-05-25 18:13:51 +0100

    Initial commit (apprentices' working code from dojo)

---
+++ test/com/bt/apprentice/TestStringCalculator.java
@@ -0,0 +1,54 @@
+package com.bt.apprentice;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestStringCalculator {
+
+	@Test
+	public void shouldReturnZeroIfPassedAnEmptyString() throws Exception {
+		assertEquals(0, StringCalculator.add(""));
+	}
+
+	@Test
+	public void shouldReturnSumOfOneNumber() throws Exception {
+		assertEquals(42, StringCalculator.add("42"));
+		assertEquals(7, StringCalculator.add("7"));
+	}
+
+	@Test
+	public void shouldReturnSumOfTwoNumbers() throws Exception {
+		assertEquals(46, StringCalculator.add("3,43"));
+	}
+
+	@Test
+	public void shouldReturnSumOfThreeOrMoreNumbers() throws Exception {
+		assertEquals(90, StringCalculator.add("30,30,30"));
+	}
+
+	@Test
+	public void shouldAllowNewLineDelimiter() throws Exception {
+		assertEquals(90, StringCalculator.add("30\n30,30"));
+	}
+
+	@Test
+	public void shouldAllowSpecificDelimiter() throws Exception {
+		assertEquals(90, StringCalculator.add("//;\n30;30;30"));
+	}
+
+	@Test
+	public void shouldAllowMinusAsDelimiter() throws Exception {
+		assertEquals(90, StringCalculator.add("//-\n30-30-30"));
+	}
+
+	@Test(expected = Exception.class)
+	public void whenNegativeNumberPassedGivesException() throws Exception {
+		try {
+			StringCalculator.add("-1987654345");
+		} catch (Exception e) {
+			assertEquals("negatives not allowed -1987654345", e.getMessage());
+			throw e;
+		}
+	}
+}
