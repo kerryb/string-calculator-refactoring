9cf40441f1f92a5dd878f7abde41c7b46458e2b5
Kerry Buckley <kerryjbuckley@gmail.com>
2013-05-27 17:02:49 +0100

    Convert tabs to two-space indentation

--- src/com/bt/apprentice/StringCalculator.java
+++ src/com/bt/apprentice/StringCalculator.java
@@ -2,26 +2,26 @@ package com.bt.apprentice;
 
 public class StringCalculator {
 
-	public static int add(String numbers) throws Exception {
-		if (numbers == "") {
-			return 0;
-		}
-		
-		String delimiter = "[,\n]";
-		
-		if (numbers.startsWith("//")){
-			delimiter = numbers.substring(2, 3);
-			numbers = numbers.substring(4);
-		}
-		
-		String[] splitNumbers = numbers.split(delimiter);
-		int sum = 0;
-		for (String number : splitNumbers) {
-			if(number.contains("-")){
-				throw new Exception("negatives not allowed " + number);
-			}
-			sum += Integer.parseInt(number);
-		}
-		return sum;
-	}
+  public static int add(String numbers) throws Exception {
+    if (numbers == "") {
+      return 0;
+    }
+
+    String delimiter = "[,\n]";
+
+    if (numbers.startsWith("//")) {
+      delimiter = numbers.substring(2, 3);
+      numbers = numbers.substring(4);
+    }
+
+    String[] splitNumbers = numbers.split(delimiter);
+    int sum = 0;
+    for (String number : splitNumbers) {
+      if (number.contains("-")) {
+        throw new Exception("negatives not allowed " + number);
+      }
+      sum += Integer.parseInt(number);
+    }
+    return sum;
+  }
 }
