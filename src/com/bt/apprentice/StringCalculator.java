3fcd1bf4281b4aeafe8f75e06a005b8e80b918a7
Kerry Buckley <kerryjbuckley@gmail.com>
2013-05-28 13:23:31 +0100

    End of demo

--- src/com/bt/apprentice/StringCalculator.java
+++ src/com/bt/apprentice/StringCalculator.java
@@ -2,26 +2,55 @@ package com.bt.apprentice;
 
 public class StringCalculator {
 
-  public static int add(String numbers) throws Exception {
-    if (numbers == "") {
+  public static int add(final String input) throws Exception {
+    Calculation calculation = new Calculation(input);
+    return calculation.performCalculation();
+  }
+}
+
+class Calculation {
+  private static final String DEFAULT_DELIMITER_PATTERN = "[,\n]";
+  private String delimiterPattern;
+  private String calculationString;
+
+  public Calculation(final String input) {
+    delimiterPattern = extractDelimiterPattern(input);
+    calculationString = extractCalculation(input);
+  }
+
+  public int performCalculation() throws Exception {
+    if (calculationString.length() == 0) {
       return 0;
     }
-
-    String delimiter = "[,\n]";
-
-    if (numbers.startsWith("//")) {
-      delimiter = numbers.substring(2, 3);
-      numbers = numbers.substring(4);
+    String[] operands = calculationString.split(delimiterPattern);
+    int total = 0;
+    for (String operand : operands) {
+      int number = Integer.parseInt(operand);
+      rejectNegativeNumber(number);
+      total += number;
     }
+    return total;
+  }
 
-    String[] splitNumbers = numbers.split(delimiter);
-    int sum = 0;
-    for (String number : splitNumbers) {
-      if (number.contains("-")) {
-        throw new Exception("negatives not allowed " + number);
-      }
-      sum += Integer.parseInt(number);
+  private void rejectNegativeNumber(int number) throws Exception {
+    if (number < 0) {
+      throw new Exception("negatives not allowed " + number);
     }
-    return sum;
+  }
+
+  private static String extractDelimiterPattern(String input) {
+    if (input.startsWith("//")) {
+      return input.substring(2, 3);
+    } else {
+      return DEFAULT_DELIMITER_PATTERN;
+    }
+  }
+
+  private static String extractCalculation(String input) {
+    if (input.startsWith("//")) {
+      return input.substring(4);
+    } else {
+      return input;
+    }
   }
 }
